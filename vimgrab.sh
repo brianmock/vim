594b169e1b080823cc159c27ffb1eb874f631892
brianmock <briannealmock@gmail.com>
2019-01-11 11:33:10 -0600

    Add most recent vim files

--- vimgrab.sh
+++ vimgrab.sh
@@ -1,4 +1,4 @@
-tar cfvz vimfiles.tgz ~/.vimrc ~/.vim ~/.tmux.conf
+tar cfvz vimfiles.tgz ~/.vimrc ~/.vim ~/.vimrc_background ~/.tmux.conf ~/.bashrc ~/.bash_profile
 git add .
 git commit -m 'Add most recent vim files'
 git push origin master
