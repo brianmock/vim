9efa5634a3d39174c84587d49865d0caef7d24db
Brian Mock <briannealmock@gmail.com>
2024-05-13 10:19:36 -0400

    Grab latest dotfiles

--- vimgrab.sh
+++ vimgrab.sh
@@ -1,4 +1,1 @@
-tar cfvz vimfiles.tgz ~/.base16_theme ~/.vimrc ~/.vim ~/.vimrc_background ~/.tmux.conf ~/.bashrc ~/.bash_profile ~/.config/base16-shell ~/tmuxrm.sh
-git add .
-git commit -m 'Add most recent vim files'
-git push origin master
+tar cfvz vimfiles.tgz ~/.base16_theme ~/.vimrc ~/.vim ~/.vimrc_background ~/.tmux.conf ~/.bashrc ~/.bash_profile ~/.config/base16-shell ~/logik.sh
